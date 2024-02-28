57057d1dfec0d5b4e01ac1563c7f2c09124073fb
yplyh125 <145650124+yplyh125@users.noreply.github.com>
2024-02-28 16:10:38 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
+$(document).ready(function(){
+	$.getJSON("index2.json",function(data){
+		
+		//할 일 처리
+		let member_data="";
+		$.each(data,function(key,value){
+			console.log(key);
+			member_data+="<tr>";
+			member_data+="<td>"+value.index+"</td>"
+			member_data+="<td>"+value.address+"</td>"
+            member_data+="<td>"+value.name+"</td>"
+			member_data+="<td>"+value.number+"</td>"
+			member_data+="</tr>";
+			
+		});
+		$("#member_table").append(member_data);
+        
+	});
+});
 
